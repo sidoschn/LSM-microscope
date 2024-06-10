756318633922009a3db6ea3a7ac456fefd04e23c
sfbarrerab <sfbarrerab@unal.edu.co>
2024-06-10 12:57:16 +0200

    Start movement from the motor taking the initial position as the cero

--- arduino/step_motor_LSM/src/motor.cpp
+++ arduino/step_motor_LSM/src/motor.cpp
@@ -3,14 +3,18 @@
 TMC2130Stepper driver = TMC2130Stepper(EN_PIN, DIR_PIN, STEP_PIN, CS_PIN);
 AccelStepper stepper = AccelStepper(stepper.DRIVER, STEP_PIN, DIR_PIN);
 
-bool dir = false;
+bool dir = true;
 int sheet_width = 200;
 long headAcceleration = 5000;
+long cero_position = 0;
 
 void init_motor() {
     
     pinMode(CS_PIN, OUTPUT);
     digitalWrite(CS_PIN, HIGH);
+
+    // Grab the initial position and set it as the cero position
+    cero_position = stepper.currentPosition();
     
     driver.begin();           
     driver.rms_current(600);    // Set stepper current to 600mA
@@ -27,7 +31,10 @@ void init_motor() {
 
 void task_move_motor(void *pvParameters) {
   command_t received_command;
+  long current_position;
   int new_sheet_width = sheet_width;
+  int first_move = true;
+
   while(1)
   {
     // If there is an element in the queue...
@@ -40,14 +47,24 @@ void task_move_motor(void *pvParameters) {
       
     }
 
+    current_position = stepper.currentPosition();
+  
     if (stepper.distanceToGo() == 0) {
-      if (dir){
-        stepper.move(sheet_width);
+      if (first_move){
+        stepper.move(sheet_width/2);
+        first_move = false;
+      }
+      else if (dir){
+        stepper.move(sheet_width);  
       }else{
         stepper.move(-1*sheet_width);
       }
-      sheet_width = new_sheet_width;
       dir = !dir;
+    }
+
+    // Update the sheet width only when it cross the cero position
+    if(current_position == cero_position){
+      sheet_width = new_sheet_width;
     }
     stepper.run();
 
