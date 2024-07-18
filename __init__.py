0352995b7b299dd92b4826aa8fca4c30e784bf29
sidoschn <dominik.schneidereit@fau.de>
2024-07-18 10:31:53 +0200

    testpush to master 2

--- __init__.py
+++ __init__.py
@@ -31,7 +31,7 @@ default_vMax = 5535
 default_lens_live_update_delay = 0.1
 default_exposure_time = int(100)
 default_position_update_delay = 0.1
-#testpush to master
+#testpush to master2
 
 
 class CameraDummy:
